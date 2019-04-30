94a5602e024a0b021406e95609909a436ee99e23
qiw-a <qiw-a@glodon.com>
2019-04-30 14:31:56 +0800

    增加springboot连接mysql数据库实例

---
+++ base-database-demo/src/main/java/com/example/base/basedatabasedemo/BaseDatabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.base.basedatabasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDatabaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseDatabaseDemoApplication.class, args);
+    }
+
+}
